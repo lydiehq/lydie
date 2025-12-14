3636becad7fe6b485041ed9d396d5ec042d3422b
Lars Salling <lars@salling.me>
2025-12-14 18:47:14 +0700

    fix(database): Move away from Bun sql client due to JSONB not being saved properly

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,10 +1,16 @@
-import { drizzle } from "drizzle-orm/bun-sql";
+import { drizzle } from "drizzle-orm/postgres-js";
 import { Resource } from "sst";
 import * as schema from "./schema";
 import { relations } from "./relations";
-import { SQL } from "bun";
+import { default as postgres } from "postgres";
+// import { SQL } from "bun";
 
-const pg = new SQL(Resource.PostgresConnectionStringDirect.value);
+// Not gonna be properly able to use Bun's SQL client before this is resolved:
+// https://github.com/drizzle-team/drizzle-orm/issues/4942 (not sure if Bun or
+// Drizzle issue)
+// const pg = new SQL(Resource.PostgresConnectionStringDirect.value);
+
+const pg = postgres(Resource.PostgresConnectionStringDirect.value);
 
 export const db = drizzle({
   client: pg,
