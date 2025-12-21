dbd51df13057ceef68ba731ec2177f499122f6fd
Lars Salling <lars@salling.me>
2025-12-21 12:17:21 +0700

    feat(landing): Better blog styling

--- packages/landing/src/utils/lydie-client.ts
+++ packages/landing/src/utils/lydie-client.ts
@@ -3,6 +3,6 @@ import { LydieClient } from "@lydie-app/sdk/client";
 export const lydieClient = new LydieClient({
   apiKey: import.meta.env.LYDIE_API_KEY,
   debug: true,
-  organizationId: "Ra8kEEDW3ywX1WiK",
+  organizationId: "WQnJeE7uYmhinDSE",
   apiUrl: "https://api.lydie.co/v1",
 });
