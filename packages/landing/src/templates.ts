0faa145df672711cc9a2ee7c3b74f3226dd50b58
Lars Salling <lars@salling.me>
2025-12-24 15:43:07 +0700

    feat(landing): Scaffold templates pages

---
+++ packages/landing/src/templates.ts
@@ -0,0 +1,7 @@
+export const templates = [
+  {
+    id: "software-developer-resume",
+    title: "Software Developer Resume Template",
+    description: "A professional resume template for software developers.",
+  },
+];
