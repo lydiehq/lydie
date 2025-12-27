5b1bd4b48412f793fabd43be4650eb9a129e976f
Lars Salling <lars@salling.me>
2025-12-27 17:44:47 +0700

    fix(landing): Remove trailing slash from blog thumbnail links

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,8 @@
 import { app } from "./api";
+import type { Serve } from "bun";
 
 export default {
   port: 3001,
   fetch: app.fetch,
-};
+  idleTimeout: 60,
+} satisfies Serve.Options<undefined>;
