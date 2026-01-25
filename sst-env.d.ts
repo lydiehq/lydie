71379df5617ec856ff8e83cc07db5d076483276d
Lars Salling <lars@salling.me>
2026-01-25 17:17:42 +0700

    feat(web): Set html theme color to our background color

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -99,11 +99,6 @@ declare module "sst" {
       "type": "sst.sst.Secret"
       "value": string
     }
-    "ReplicationManager": {
-      "service": string
-      "type": "sst.aws.Service"
-      "url": string
-    }
     "ShopifyClientId": {
       "type": "sst.sst.Secret"
       "value": string
