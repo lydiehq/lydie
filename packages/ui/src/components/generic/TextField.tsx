4f4fea0f4efec8b7236b29426e78381bd5a59b37
Lars Salling <lars@salling.me>
2026-01-28 13:18:18 +0700

    feat: Generic ui TextField component

--- packages/ui/src/components/generic/TextField.tsx
+++ packages/ui/src/components/generic/TextField.tsx
@@ -4,12 +4,10 @@ import {
   type ValidationResult,
 } from "react-aria-components";
 
-import { useFieldContext } from "@/hooks/use-app-form";
+import { Description, FieldError, Input, Label, TextArea } from "./Field";
+import { composeTailwindRenderProps, focusRing } from "./utils";
 
-import { Description, FieldError, Input, Label, TextArea } from "@lydie/ui/components/generic/Field";
-import { composeTailwindRenderProps, focusRing } from "@lydie/ui/components/generic/utils";
-
-export interface TextFieldProps extends Omit<AriaTextFieldProps, "name"> {
+export interface TextFieldProps extends AriaTextFieldProps {
   label?: string;
   description?: string;
   placeholder?: string;
@@ -29,17 +27,11 @@ export function TextField({
   descriptionClassName,
   ...props
 }: TextFieldProps) {
-  const field = useFieldContext<string>();
-
   return (
     <AriaTextField
       {...props}
-      value={field.state.value}
-      onChange={(value) => field.handleChange(value)}
-      onBlur={field.handleBlur}
       className={composeTailwindRenderProps(props.className, "flex flex-col gap-1")}
       validationBehavior="aria"
-      isInvalid={field.state.meta.errors.length > 0}
     >
       {label && <Label className={labelClassName}>{label}</Label>}
       {textarea ? (
@@ -48,9 +40,7 @@ export function TextField({
         <Input placeholder={placeholder} className={focusRing} />
       )}
       {description && <Description className={descriptionClassName}>{description}</Description>}
-      <FieldError>
-        {field.state.meta.errors.length > 0 ? field.state.meta.errors.join(", ") : errorMessage}
-      </FieldError>
+      <FieldError>{errorMessage}</FieldError>
     </AriaTextField>
   );
 }
