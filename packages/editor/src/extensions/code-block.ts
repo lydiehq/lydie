e4a0510d68c3c736ce106b409e4030a662ff3135
Lars Salling <lars@salling.me>
2026-02-08 14:33:52 +0700

    fix(web): Don't apply inline code style to code blocks

--- packages/editor/src/extensions/code-block.ts
+++ packages/editor/src/extensions/code-block.ts
@@ -3,20 +3,16 @@ import type { NodeViewRenderer } from "@tiptap/core";
 import { CodeBlockLowlight } from "@tiptap/extension-code-block-lowlight";
 import { all, createLowlight } from "lowlight";
 
-// Create a lowlight instance with all languages loaded
 const lowlight = createLowlight(all);
 
 export interface CodeBlockOptions {
-  // Allow overriding lowlight if needed, but default to our instance
-  lowlight?: typeof lowlight;
   addNodeView?: () => NodeViewRenderer;
 }
 
 export const CodeBlock = CodeBlockLowlight.extend<CodeBlockOptions>({
   addOptions() {
     return {
-      ...this.parent?.(),
-      lowlight: lowlight,
+      lowlight,
       addNodeView: undefined,
     };
   },
